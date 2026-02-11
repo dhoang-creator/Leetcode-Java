977be842a4bd05a090a08f9809d7e0d40dad8415
duyhoang <duyhoang123@hotmail.co.uk>
2026-02-11 20:59:43 +0000

    Longest Substring without Repeat LeetCode Test Suite added

--- src/main/java/org/example/lengthOfLongestSubstringOptimal.java
+++ src/main/java/org/example/lengthOfLongestSubstringOptimal.java
@@ -1,7 +1,8 @@
 import java.util.HashSet;
 import java.util.Set;
 
-public int lengthOfLongestSubstringOptimal(String s) {
+// made static due to main method calling this class
+public static nt lengthOfLongestSubstringOptimal(String s) {
     Set<Character> set = new HashSet<>();
 
     int left = 0;
@@ -19,4 +20,24 @@ public int lengthOfLongestSubstringOptimal(String s) {
     }
 
     return maxLength;
+}
+
+// getting into the habit of adding a test suite -> could we tidy up the below at some point?
+public static void main(String[] args) {
+
+    test("abcabcbb", 3);
+    test("bbbbb", 1);
+    test("pwwkew", 3);
+    test("", 0);
+    test("a", 1);
+    test("abba", 2);
+    test("dvdf", 3);
+    test("tmmzuxt", 5);
+}
+
+private static void test(String input, int expected) {
+    int result = lengthOfLongestSubStringOptimal(input);
+    System.out.println(
+            "Input : \"" + input + "\" | Expected: " + expected + " | Got: " + result
+    );
 }
