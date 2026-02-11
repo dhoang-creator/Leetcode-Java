c549a86f9dac88c37617994ff772aaba783c5172
duyhoang <duyhoang123@hotmail.co.uk>
2026-02-11 21:29:48 +0000

    Maximum Average Subarray Test Suite added

--- src/main/java/org/example/MaximumAverageSubarrayOptimal.java
+++ src/main/java/org/example/MaximumAverageSubarrayOptimal.java
@@ -21,4 +21,23 @@ public class MaximumAverageSubarrayOptimal {
 
         return (double) maxSum / k;
     }
+
+    public static void main(String[] args) {
+
+        test(new int[]{1, 12, -5, -6, 50, 3}, 4, 12.75);
+        test(new int[]{5}, 1, 5.0);
+        test(new int[]{0, 0, 0, 0}, 2, 0.0);
+        test(new int[]{-1, -12, -5, -6, -50, -3}, 2, -5.5);
+        test(new int[]{3, 3, 3, 3}, 4, 3.0)
+    }
+
+    private static void test(int[] nums, int k, double expected) {
+        double result = findMaxAverage(nums, k);
+
+        if (Math.abs(result - expected) < 0.00001) {
+            System.out.println("PASS");
+        } else {
+            System.out.println("FAIL | Expected: " + expect + " | Got: " + result);
+        }
+    }
 }
