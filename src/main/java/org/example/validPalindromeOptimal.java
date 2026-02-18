72445a4599e858c635f75c5f7caf09ac4e07754e
duyhoang <duyhoang123@hotmail.co.uk>
2026-02-18 17:17:59 +0000

    Valid Palindrome non-TLE added

--- src/main/java/org/example/validPalindromeOptimal.java
+++ src/main/java/org/example/validPalindromeOptimal.java
@@ -20,3 +20,27 @@ public boolean isPalindrome(String s) {
     }
     return true; // O(1)
 }
+
+public boolean isPalindromeCorrect(String s) {
+    if (s.isEmpty()) {
+        return true;
+    }
+    int start = 0;
+    int last = s.length() -1;
+    while (start <= last) {
+        char currFirst = s.charAt(start);
+        char currLast = s.charAt(last);
+        if (!Character.isLetterOrDigit(currFirst)) {
+            start++;
+        } else if (!Character.isLetterOrDigit(currLast)) {
+            last--;
+        } else {
+            if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
+                return false;
+            }
+            start++;
+            last--;
+        }
+    }
+    return true;
+}
