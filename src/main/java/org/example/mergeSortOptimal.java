e0d18d80175b281c8a90edcf7ae790bca394f559
duyhoang <duyhoang123@hotmail.co.uk>
2026-02-18 20:28:32 +0000

    Merge Sorted Array optimal added

---
+++ src/main/java/org/example/mergeSortOptimal.java
@@ -0,0 +1,16 @@
+public static void merge(int[] nums1, int m, int[] nums2, int n) {
+    int p1 = m - 1; // Pointer for nums1 // O(1)
+    int p2 = n - 1; // Pointer for nums2 // O(1)
+    int p = m + n - 1; // Pointer for placement in nums1 // O(1)
+
+    while (p2 >= 0) {
+        if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+            nums1[p] = nums1[p1];
+            p1--;
+        } else {
+            nums1[p] = nums2[p2];
+            p2--;
+        }
+        p--;
+    }
+}
