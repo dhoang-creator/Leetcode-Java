60c48e06769224c46667f744d2932a449e321775
duyhoang <duyhoang123@hotmail.co.uk>
2026-02-26 17:55:21 +0000

    minStack added

--- src/main/java/org/example/rangeSumQuery.java
+++ src/main/java/org/example/rangeSumQuery.java
@@ -23,7 +23,7 @@ package org.example;
 
 class rangeSumQuery { 
     int[] prefix;
-    public NumArray(int[] nums) {
+    public void NumArray(int[] nums) {
         prefix = new int[nums.length];
         prefix[0] = nums[0];
         for (int i = 1; i < nums.length; i++) {
